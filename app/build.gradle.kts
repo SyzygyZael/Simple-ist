0f82eb9f7591f7a8c2f720add17c49b62f60a483
SyzygyZael <kevnes522@gmail.com>
2026-07-09 14:52:18 +0700

    - Purchases are now instantly acknowledged (hopefully)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.kevinnesbitt.simple_ist"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "simple-ist_1.3"
+        versionCode = 6
+        versionName = "simple-ist_1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
